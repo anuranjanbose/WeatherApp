8b5ba7fbdc78d151a997f204774e3e7ebd613be4
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-07 16:37:17 +0530

    Weather custom cell added, hard-code value displayed

---
+++ WeatherApp/Custom Cells/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  WeatherApp
+//
+//  Created by Anuranjan Bose on 07/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
