f9dda8f1071b86ce6cf06cd90e7c69f9cb8cdf5c
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-07 23:08:46 +0530

    SettingsViewModel added, Unit enum added

---
+++ WeatherApp/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewModel.swift
+//  WeatherApp
+//
+//  Created by Anuranjan Bose on 07/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+    case celsius = "metric"
+    case fahrenheit = "imperial"
+}
+
+extension Unit {
+    var displayName: String {
+        switch self {
+        case .celsius:
+            return "Celsius"
+        case .fahrenheit:
+            return "Fahrenheit"
+        }
+    }
+}
+
+struct SettingsViewModel {
+    
+    let units = Unit.allCases
+}
